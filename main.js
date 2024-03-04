5bb45ffd6a3575dfd9db81f29c647883956c4da8
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-03-04 11:27:03 +0100

    Fixed fetch method for json data

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 Vue.createApp({
-    methods: {
+    methods: { 
+      fetchData() {
+        fetch ('./season.json')
+        .then (response => {
+          return response.json();
+        })
+        .then(data => {
+          this.teams = data.teams;
+        })
+
+      },
+      calculateGoalDifference(team) {
+        return team.goals_for - team.goals_against;
+   },
+   calculatePoints(team) {
+       return team.wins * 3 + team.draws;
+   }
+
     },
     computed: {
     },
     data() {
         return {
+          teams: []   
         };
+      },
+
+      mounted() {
+        this.fetchData();
     }
-}).mount('#app');
+      
+    }).mount('#app');
